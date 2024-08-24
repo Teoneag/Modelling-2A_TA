88e380125ea9ffb2b2abc22fbc39548d82b68f07
Teoneag <teodor.neagoe.climber@gmail.com>
2024-08-24 16:42:41 +0300

    5 - working visualization-0

--- Modeling2A.py
+++ Modeling2A.py
@@ -4,42 +4,42 @@ import time
 import pandas as pd
 
 def travelTime(freeflow, numvehicles, capacity, alpha, beta):
-    return freeflow * (1 + alpha * (numvehicles / capacity) ** beta) + np.random.normal(0, 2)
+    return freeflow*(1+alpha*(numvehicles/capacity)**beta) + np.random.normal(0,2)
 
 def capacity(l_car, d_spacing, lanes, length):
-    return math.floor((lanes * length) / (l_car + d_spacing))
+    return math.floor((lanes*length)/(l_car + d_spacing))
 
-def freeflow(length, v_limit):
-    return (length / v_limit) * (60 / 1000)
+def freeflow(length,v_limit):
+    return (length/v_limit)*(60/1000)
 
 def probability_numerator(edge):
     N_uv = edges_vehicles[edge]
     total_time = 0
     if N_uv == 0:
-        return 1 / edge_info[edge][0]
-    if N_uv >= edge_info[edge][1]:  # Makes sure the probability is 0 if capacity is at a maximum
-        return 0
+        return 1/edge_info[edge][0]
+    if N_uv >= edge_info[edge][1]: #makes sure the probability is 0 if capacity is at a maximum
+        return 0 
     for cars in vehicles:
-        if vehicles[cars][1] == edge:
+        if vehicles[cars][1] == edge: 
             total_time += vehicles[cars][2]
-    return 1 / ((1 / N_uv) * total_time)
+    return 1/((1/N_uv)*total_time)
 
-def probability_denominator(node, initializing):
+def probability_denominator(node,initializing):
     total = 0
     for edge in node_out_edges[node]:
         if edges_vehicles[edge] == 0:
-            total += 1 / edge_info[edge][0]  # Using freeflow if roads are empty
+            total += 1/edge_info[edge][0] #Using freeflow if roads are empty
         elif edges_vehicles[edge] >= edge_info[edge][1]:
             if initializing == 1:
-                edge_info[edge][2] += 1  # Congestion in terms of cars that can't enter the road counter
+                edge_info[edge][2] += 1 # Congestion in terms of cars that cant enter the road counter
         else:
             total += probability_numerator(edge)
     if total == 0:
         return 1
     return total
 
-def probability(node, edge, initializing):
-    return probability_numerator(edge) / probability_denominator(node, initializing)
+def probability(node,edge,initializing):
+    return probability_numerator(edge)/probability_denominator(node,initializing)
 
 l_car = 4.5
 d_spacing = 55
@@ -47,106 +47,75 @@ alpha = 0.15
 beta = 4
 
 # [Length, V_lim, lanes]
-edge_data = {
-    "1_A": [30000, 100, 2], "1_B": [50000, 100, 2], "A_C": [40000, 100, 2], "B_C": [60000, 100, 2],
-    "C_D": [20000, 100, 2], "C_E": [30000, 100, 2], "C_2": [70000, 100, 2], "D_2": [40000, 100, 2],
-    "E_2": [50000, 100, 2]
-}
-edges_vehicles = {key: 0 for key in edge_data.keys()}  # Initializes all edges with 0 vehicles
+edge_data = {"1_A": [30000,100,2], "1_B": [50000,100,2],"A_C": [40000,100,2],"B_C": [60000,100,2],"C_D":
+              [20000,100,2],"C_E": [30000,100,2],"C_2": [70000,100,2],"D_2": [40000,100,2],"E_2": [50000,100,2]}
+edges_vehicles = {"1_A": 0, "1_B": 0,"A_C": 0,"B_C": 0,"C_D": 0,"C_E": 0,"C_2": 0,"D_2": 0,"E_2": 0}
 
-# Possible other nodes
-node_out_edges = {
-    "1": ["1_A", "1_B"], "A": ["A_C"], "B": ["B_C"], "C": ["C_D", "C_E", "C_2"],
-    "D": ["D_2"], "E": ["E_2"]
-}
+# Possible other nodes 
+node_out_edges = {"1": ["1_A","1_B"], "A":["A_C"], "B": ["B_C"], "C": ["C_D","C_E","C_2"], "D": ["D_2"], "E": ["E_2"]}
+nodes_vehicles = {"1": 0, "A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "2": 0}
 
-# Setting up information of the edges
+# setting up information of the edges
 edge_info = {}
-vehicles = {}
+vehicles = {}   
 edge_times = {}
-
-for key in edge_data.keys():  # This sets up dictionaries for edge info with freeflow/capacity data, [2] is congestion amount
-    data = edge_data[key]  # Edge_times is simply a dictionary of lists in which all travel times are saved for saving to excel later.
-    edge_info[key] = [freeflow(data[0], data[1]), capacity(l_car, d_spacing, data[2], data[0]), 0]
+traffic_log = []  # To log timestamp, vehicles on edges, and vehicles on vertices
+for key in edge_data.keys():    # This sets up dictionaries for edge info with freeflow/capacity data, [2] is congestion amount
+    data = edge_data[key]       # Edge_times is simply a dictionary of lists in which all travel times are saved for saving to excel later.
+    edge_info[key] = [freeflow(data[0],data[1]),capacity(l_car, d_spacing,data[2],data[0]),0]
     edge_times[key] = []
 
 vehicle_id = 0
+start_time = time.time()
 peak_hour = False
 initializing = 0
-time_step_data = []
 
-# Start the simulation timer
-start_time = time.time()
-
-for min in range(1, 241):
-    if min > 120:
+for min in range(1,241):
+    if min > 120: 
         initializing = 1
     print(min)
-
-    # Reset waiting vehicles count at each node for this time step
-    waiting_vehicles_at_node = {node: 0 for node in node_out_edges.keys()}
-
-    incoming_traffic = round(np.random.normal(85, 1.5))
+    incoming_traffic = round(np.random.normal(85,1.5))
     if peak_hour:
-        incoming_traffic = round(np.random.normal(95, 1))
-    
-    for car in range(1, incoming_traffic + 1):
+        incoming_traffic = round(np.random.normal(95,1))
+    for car in range(1,incoming_traffic+1):
         vehicle_id += 1
-        vehicles[vehicle_id] = [initializing, "1", 0, 0, "", 0, 0, min]  # Adding the entry time to City 1
-
+        vehicles[vehicle_id] = [initializing, "1", 0, 0, "", 0, 0, ""] # part of initiliazation or not, Current place, time left on road, total time spent, route 
+        nodes_vehicles["1"] += 1  # Increment the vehicle count for node "1"
     for car in vehicles:
         if vehicles[car][1] != "2":
-            vehicles[car][3] += 1  # Adds minute to total expired time if vehicle has not arrived at City 2
-        
-        if vehicles[car][1] in node_out_edges.keys():  # Checks if the vehicle is currently at a node in between edges
+            vehicles[car][3] += 1 #adds minute to total expired time if vehicle has not arrived at city 2 
+        if vehicles[car][1] in node_out_edges.keys(): # Checks if  the vehicle is currently at a node inbetween edges
             prob_list = []
-            for out_edge in node_out_edges[vehicles[car][1]]:
-                prob_list.append(probability(vehicles[car][1], out_edge, initializing))
-            
+            for out_edge in node_out_edges[vehicles[car][1]]:  
+                prob_list.append(probability(vehicles[car][1],out_edge,initializing))
             if sum(prob_list) < 0.99:
                 vehicles[car][6] += 1
-                waiting_vehicles_at_node[vehicles[car][1]] += 1
+                vehicles[car][7] += vehicles[car][1]
                 continue
-            
-            next_edge = np.random.choice(node_out_edges[vehicles[car][1]], p=prob_list)  # Stochastically pick next edge
-            vehicles[car][1] = next_edge  # Updates where vehicle is currently
-            vehicles[car][4] += (next_edge + " ")  # Updates road taken
-            tt = travelTime(edge_info[next_edge][0], edges_vehicles[next_edge], edge_info[next_edge][1], alpha, beta)
+            next_edge = np.random.choice(node_out_edges[vehicles[car][1]],p = prob_list) #stochastically pick next edge based on probabilties defined in functions at the start
+            nodes_vehicles[vehicles[car][1]] -= 1  # Decrement the vehicle count for the current node
+            vehicles[car][1] = next_edge #updates where vehicle is currently
+            vehicles[car][4] += (next_edge + " ") #updates road taken
+            tt = travelTime(edge_info[next_edge][0],edges_vehicles[next_edge],edge_info[next_edge][1],alpha,beta)
             vehicles[car][5] += edge_info[next_edge][0]
-            vehicles[car][2] = tt  # Updates travel time
-            
-            if initializing == 1:
-                edge_times[next_edge].append(tt)
+            vehicles[car][2] = tt #updates travel time
+            if initializing == 1: 
+                edge_times[next_edge].append(tt) 
             edges_vehicles[next_edge] += 1
+        if vehicles[car][1] not in node_out_edges.keys() and vehicles[car][1] != "2": # checks if vehicle is at an edge
+            vehicles[car][2] -= 1 # takes a minute of the travel time
+            if vehicles[car][2] < 0: # checks if travel time has expired. 
+                edges_vehicles[vehicles[car][1]] -= 1 #remove vehicle from edge amount counter
+                next_node = vehicles[car][1].split("_")[1]
+                vehicles[car][1] = next_node #updates vehicle position
+                nodes_vehicles[next_node] += 1  # Increment the vehicle count for the new node
 
-        if vehicles[car][1] not in node_out_edges.keys() and vehicles[car][1] != "2":  # Checks if vehicle is at an edge
-            vehicles[car][2] -= 1  # Takes a minute of the travel time
-            if vehicles[car][2] < 0:  # Checks if travel time has expired
-                edges_vehicles[vehicles[car][1]] -= 1  # Remove vehicle from edge amount counter
-                vehicles[car][1] = vehicles[car][1].split("_")[1]  # Updates vehicle position
+    # Log the traffic data for the current minute
+    log_entry = {"TimeStamp": min}
+    log_entry.update(edges_vehicles.copy())  # Add the edge data to the log
+    log_entry.update(nodes_vehicles.copy())  # Add the node data to the log
+    traffic_log.append(log_entry)
 
-    # Collect data for the current time step
-    step_data = {
-        'time': min,
-        'edges': {},
-        'waiting_at_nodes': waiting_vehicles_at_node.copy(),
-    }
-
-    for edge in edges_vehicles:
-        occupancy = edges_vehicles[edge]
-        capacity = edge_info[edge][1]
-        percentage_used = (occupancy / capacity) * 100
-
-        step_data['edges'][edge] = {
-            'occupancy': occupancy,
-            'capacity': capacity,
-            'percentage_used': percentage_used
-        }
-
-    # Save the data for this time step
-    time_step_data.append(step_data)
-
-# End the simulation timer and print duration
 print("Simulation Time: --- %s seconds ---" % (time.time() - start_time))
 
 travel_time = []
@@ -155,26 +124,24 @@ freeflow_time = []
 congestions = []
 minutes_waited = []
 where_waited = []
-entry_times = []
-
-for car in vehicles:
+for car in vehicles: 
     if vehicles[car][0] == 1 and vehicles[car][1] == "2":
         travel_time.append(vehicles[car][3])
         route.append(vehicles[car][4])
         freeflow_time.append(vehicles[car][5])
         minutes_waited.append(vehicles[car][6])
         where_waited.append(vehicles[car][7])
-        entry_times.append(vehicles[car][7])
 
 for edge in edge_info:
     congestions.append(edge_info[edge][2])
 
-df1 = pd.DataFrame({'Total Travel Time': travel_time, 'Route': route, 'Freeflow Time': freeflow_time, 
-                    'Minutes Waited': minutes_waited, 'Where?': where_waited, 'Entry Time': entry_times})
-df2 = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in edge_times.items()]))
-df3 = pd.DataFrame({"Edge": edge_info.keys(), "Individual Car Congestions": congestions})
+df1 = pd.DataFrame({'Total Travel Time': travel_time, 'Route': route, 'Freeflow Time': freeflow_time, 'Minutes Waited': minutes_waited, 'Where?': where_waited})
+df2 = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in edge_times.items() ]))
+df3 = pd.DataFrame({"Edge": edge_info.keys(), "Individual Car Congestions": congestions })
+df4 = pd.DataFrame(traffic_log)  # Create a DataFrame for the traffic log
 
 with pd.ExcelWriter("test.xlsx") as writer:
-    df1.to_excel(writer, sheet_name="Sheet1")
-    df2.to_excel(writer, sheet_name="Sheet2")
-    df3.to_excel(writer, sheet_name="Sheet3")
+    df1.to_excel(writer, sheet_name = "Sheet1")
+    df2.to_excel(writer, sheet_name = "Sheet2")
+    df3.to_excel(writer, sheet_name = "Sheet3")
+    df4.to_excel(writer, sheet_name = "Traffic Log")  # Save the traffic log to a new sheet
--- visualization.py
+++ visualization.py
@@ -1,91 +1,95 @@
 import pygame
 import pandas as pd
+import time
 
-# Pygame setup
+# Load the traffic log from the previous simulation
+df_traffic_log = pd.read_excel("test.xlsx", sheet_name="Traffic Log")
+
+# Initialize Pygame
 pygame.init()
 
-# Screen dimensions
-width, height = 1000, 600
+# Set up display
+width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Traffic Simulation")
 
-# Colors
+# Define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
-# Frame rate
-clock = pygame.time.Clock()
-fps = 30
-
-# Load data from the Excel file
-df = pd.read_excel("test.xlsx", sheet_name="Sheet4")
-# Assuming df has columns 'Vehicle ID', 'Time', 'Position'
-
-# Mapping nodes to screen coordinates (example)
+# Set up node positions
 node_positions = {
     "1": (100, 300),
-    "A": (300, 200),
-    "B": (300, 400),
-    "C": (500, 300),
-    "D": (700, 200),
-    "E": (700, 400),
-    "2": (900, 300),
+    "A": (200, 200),
+    "B": (200, 400),
+    "C": (400, 300),
+    "D": (600, 200),
+    "E": (600, 400),
+    "2": (700, 300)
 }
 
-# Vehicle data storage (could be improved)
-vehicle_positions = {}
+# Define edges as pairs of nodes
+edges = {
+    "1_A": ("1", "A"),
+    "1_B": ("1", "B"),
+    "A_C": ("A", "C"),
+    "B_C": ("B", "C"),
+    "C_D": ("C", "D"),
+    "C_E": ("C", "E"),
+    "C_2": ("C", "2"),
+    "D_2": ("D", "2"),
+    "E_2": ("E", "2")
+}
 
-# Main loop
+# Function to draw the graph
+def draw_graph(traffic_data):
+    screen.fill(WHITE)
+
+    # Draw edges
+    for edge, (start, end) in edges.items():
+        pygame.draw.line(screen, BLACK, node_positions[start], node_positions[end], 5)
+        
+        # Display the number of cars on the edge
+        mid_point = (
+            (node_positions[start][0] + node_positions[end][0]) // 2,
+            (node_positions[start][1] + node_positions[end][1]) // 2
+        )
+        cars_on_edge = traffic_data[edge]
+        font = pygame.font.Font(None, 36)
+        text = font.render(str(cars_on_edge), True, RED)
+        screen.blit(text, mid_point)
+
+    # Draw nodes (vertices)
+    for node, pos in node_positions.items():
+        pygame.draw.circle(screen, BLUE, pos, 20)
+
+        # Display the number of cars on the vertex
+        cars_on_node = traffic_data[node]
+        font = pygame.font.Font(None, 36)
+        text = font.render(str(cars_on_node), True, RED)
+        screen.blit(text, (pos[0] + 25, pos[1] - 25))
+
+    pygame.display.flip()
+
+# Main loop to animate traffic based on the log data
 running = True
-time_step = 1  # Start time
+index = 0
+clock = pygame.time.Clock()
 
 while running:
-    screen.fill(WHITE)  # Clear screen
-    
-    # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    # Draw edges
-    pygame.draw.line(screen, BLACK, node_positions["1"], node_positions["A"], 5)
-    pygame.draw.line(screen, BLACK, node_positions["1"], node_positions["B"], 5)
-    pygame.draw.line(screen, BLACK, node_positions["A"], node_positions["C"], 5)
-    pygame.draw.line(screen, BLACK, node_positions["B"], node_positions["C"], 5)
-    pygame.draw.line(screen, BLACK, node_positions["C"], node_positions["D"], 5)
-    pygame.draw.line(screen, BLACK, node_positions["C"], node_positions["E"], 5)
-    pygame.draw.line(screen, BLACK, node_positions["D"], node_positions["2"], 5)
-    pygame.draw.line(screen, BLACK, node_positions["E"], node_positions["2"], 5)
 
-    # Draw nodes
-    for node, pos in node_positions.items():
-        pygame.draw.circle(screen, BLACK, pos, 10)
-        font = pygame.font.Font(None, 36)
-        text = font.render(node, True, BLACK)
-        screen.blit(text, (pos[0] + 15, pos[1] - 15))
-    
-    # Update vehicle positions
-    current_positions = df[df['Time'] == time_step]
-    for _, row in current_positions.iterrows():
-        vehicle_id = row['Vehicle ID']
-        position = row['Position']
-        
-        if position in node_positions:
-            vehicle_positions[vehicle_id] = node_positions[position]
-    
-    # Draw vehicles
-    for pos in vehicle_positions.values():
-        pygame.draw.circle(screen, RED, pos, 5)
-    
-    # Update the display
-    pygame.display.flip()
-    
-    # Increment time step and control frame rate
-    time_step += 1
-    clock.tick(fps)
-    
-    # Stop the loop after the last time step
-    if time_step > df['Time'].max():
-        running = False
+    # Get the traffic data for the current timestamp
+    if index < len(df_traffic_log):
+        traffic_data = df_traffic_log.iloc[index].to_dict()
+        draw_graph(traffic_data)
+        index += 1
+        time.sleep(0.1)  # Delay for animation effect
+
+    clock.tick(60)  # Control the frame rate
 
 pygame.quit()
